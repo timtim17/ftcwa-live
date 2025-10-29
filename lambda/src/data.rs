01db257ebabc1e24054509ec1c45d1e23f5572ba
Jason Cheng <ckaiwen3@hotmail.com>
2025-10-28 18:45:50 -0700

    plus Knuth

--- lambda/src/data.rs
+++ lambda/src/data.rs
@@ -102,7 +102,8 @@ pub const ALL_STREAMS: phf::Map<&'static str, phf::Map<&'static str, &'static st
         "bardeen" => "https://youtube.com/live/3qVxio14lTs",
         "noddack" => "https://youtube.com/live/UN1wMNuX9IU",
         "salk" => "https://youtube.com/live/CdzKeENztQs",
-        "turing" => "https://youtube.com/live/Ixm2h2kHJSM"
+        "turing" => "https://youtube.com/live/Ixm2h2kHJSM",
+        "knuth" => "https://youtube.com/live/Zk3hcROvo1g"
     },
 };
 
